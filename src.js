dde2e20e997ace815facda0edc71c190a6c4b813
GlitchHunterCoder <209635473+GlitchHunterCoder@users.noreply.github.com>
2026-04-06 11:42:31 +0100

    Update src.js

--- src.js
+++ src.js
@@ -1,18 +1,35 @@
 class Realm {
+  static TRAPS = [
+    'get',
+    'set',
+    'has',
+    'deleteProperty',
+    'defineProperty',
+    'getOwnPropertyDescriptor',
+    'ownKeys',
+    'getPrototypeOf',
+    'setPrototypeOf',
+    'isExtensible',
+    'preventExtensions',
+    'apply',
+    'construct',
+  ];
 
   constructor({ scopes, global, travel }) {
-    this.scopes = scopes;  // { name: container object }
-    this.global = global;  // the tree of references
-    this.travel = travel;  // traversal logic
+    this.scopes = scopes;
+    this.global = global;
+    this.travel = travel;
 
-    const self = this;
-    const proxy = new Proxy(Object.create(null), {
-      get(_, key)         { return self.travel({ op: 'get', key, node: self.global, realm: self }); },
-      set(_, key, value) { self.travel({ op: 'set', key, value, node: self.global, realm: self }); return true; },
-      has(_, key)         { return self.travel({ op: 'has', key, node: self.global, realm: self }) ?? false; },
-    });
+    const handler = Object.fromEntries(
+      Realm.TRAPS.map(op => [
+        op,
+        (target, ...args) =>
+          this.travel({ op, target, args, node: this.global, realm: this })
+          ?? Reflect[op](target, ...args)
+      ])
+    );
 
-    Object.setPrototypeOf(globalThis, proxy);
+    Object.setPrototypeOf(globalThis, new Proxy(Object.create(null), handler));
   }
 
   container(name) {
