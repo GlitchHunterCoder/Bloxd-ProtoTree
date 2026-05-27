a856f5fe33618d9eac2a461161ff3c6cc137174a
GlitchHunterCoder <209635473+GlitchHunterCoder@users.noreply.github.com>
2026-05-27 13:02:11 +0100

    Update src.js

--- src.js
+++ src.js
@@ -89,16 +89,19 @@ let Realm = class {
         return (!Realm.active || _active) ? undefined : handler[prop]
       }
     });
-    ["Array","Object","String","Number","Boolean","RegExp","Function"].forEach(key => {
+      
+    ["String","Number","Boolean","RegExp","Function"].forEach(key => {
         Reflect.setPrototypeOf(globalThis[key].prototype, new Proxy(snapshot(globalThis[key].prototype), HANDLE))
-        Reflect.ownKeys(globalThis[key].prototype).filter(k=>key === "Array" && k !== "push").forEach(k => {
+        Reflect.ownKeys(globalThis[key].prototype).forEach(k => {
         try { Reflect.deleteProperty(globalThis[key].prototype, k) } catch(e) {}
         })
     });
+
     Reflect.setPrototypeOf(globalThis, new Proxy(snapshot(globalThis), HANDLE))
     Reflect.ownKeys(globalThis).filter(k=>k !== "globalThis").forEach(k => {
       try { Reflect.deleteProperty(globalThis, k) } catch(e) {}
     });
+      
     Realm.ONE = true
     _active = false
     Realm.active = true
